6319db7d2b2a94f6e969dd6ddf46f4e84481c06f
100Ubuntu <potterd123@gmail.com>
2022-11-07 18:49:10 -0800

    signup/make user done with no backend connection

--- driver.py
+++ driver.py
@@ -1,7 +1,30 @@
 import sys
+from userController import checkIfUserExist , createUser
 
-def main(args):
-    print(args)
+def main():
+    print("enter q to quit at any time")
+    args = input("login or signup? ")
+    try:
+        while args[0] != "q":
+            print(args)
+            if(args == "signup"):
+                username = input("input username: ")
+                if checkIfUserExist(username):
+                    print("this user exisit, try restarting and logging in")
+                    continue
+                password = input("new password: ")
+                passConfirm = input("confirm password: ")
+                while(password != passConfirm):
+                    print("passwords do not match...")
+                    password = input("new password: ")
+                    passConfirm = input("confirm password: ")
+                createUser(username,password)
+            
+            args = input("next?")
+                
+                
+    except:
+        print("error: ")
 
 
-main(sys.argv)
+main()
