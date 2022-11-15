d5f62b7ee5b6c2a543e02f2f88688f3029868fab
Brandon Huynh <brandon.huynh@live.com>
2022-11-14 16:32:55 -0800

    Abstracted database connection into connection.py

--- backendDriver.py
+++ backendDriver.py
@@ -1,41 +1,14 @@
-import sys
 import sqlalchemy
-import urllib.parse
-import os
-from dotenv import load_dotenv
+from connection import create_connection
+from goodRoutes import good_id_by_name, menu_items
 
 
 def main():
-       
-    # Load env file
-    load_dotenv()
+    conn = create_connection()
 
-    # Connection Parameters (you will sub in with your own databases values)
-    escapedPassword = urllib.parse.quote_plus(os.environ.get("DB_PASSWORD"))
-    sqldialect = os.environ.get("DB_DIALECT")
-    username = os.environ.get("DB_USER")
-    database = os.environ.get("DB_NAME")
-    host = os.environ.get("DB_HOST")
-
-    # Build the connection string based on database specific parameters
-    connectionString = f"{sqldialect}://{username}:{escapedPassword}@{host}/{database}"
-
-    # Create a new DB engine based on our connection string
-    engine = sqlalchemy.create_engine(connectionString)
-
-    # Create a single connection to the database. Later we will discuss pooling connections.
-    conn = engine.connect()
-
-    # The sql we want to execute
-    sql = """
-    show tables
-    """
-
-    # Run the sql and returns a CursorResult object which represents the SQL results
+    sql = """ desc users """
     result = conn.execute(sqlalchemy.text(sql))
 
-    # Iterate over the CursorResult object row by row and just print.
-    # In a real application you would access the fields directly.
     for row in result:
         print(row)
 
