a30c48bac296227ee7c290e00797e55bc72ab31e
100Ubuntu <potterd123@gmail.com>
2022-11-15 18:45:14 -0800

    added login and create user

--- userController.py
+++ userController.py
@@ -1,10 +1,42 @@
-def checkIfUserExist(username):
-    print("checking if user exist")
+import sqlalchemy
+from connection import get_connection
+import hashlib
+   
+    # conn = get_connection()
+    # sql = "SELECT * FROM goods WHERE Name = \"%s\"" % item
 
-def createUser(username, password): # create and return the user object created
-    print("creating " + username + "...")
-    return 12345
+    # result = conn.execute(sqlalchemy.text(sql)).first()[0]
 
-def attemptLogin(username, password): #will attempt to login with current credentials and either throw an error if no user exisits
-    print("attempting login")          # or will return user object
-    return 12345                        
+    # return result
+
+def checkIfUserExist(email):
+    conn = get_connection()
+    sql = sqlalchemy.text("SELECT COUNT(*) FROM users WHERE email = :e").bindparams(e = email)
+    result = conn.execute(sql).first()[0]
+    if(result):
+        return True
+    return False
+
+def createUser(email, password): # create and return the user object created
+    hashedPass = hashlib.sha256(password.encode("utf-8")).hexdigest()
+    print("creating " + email + "...")
+    conn = get_connection()
+    sql = sqlalchemy.text("insert into users (email,password) values (:e, :p)").bindparams(e = email, p = hashedPass)
+    result = conn.execute(sql)
+    
+    return result
+def deleteUser(email):
+    conn = get_connection()
+    sql = sqlalchemy.text("delete from users where email = :e").bindparams(e = email)
+    result = conn.execute(sql)
+
+
+def attemptLogin(email, password): #will attempt to login with current credentials and either throw an error if no user exisits
+    conn = get_connection()
+    sql = sqlalchemy.text("select email from users where email = :e and password = :p").bindparams(e = email, p = hashlib.sha256(password.encode("utf-8")).hexdigest())
+    result = conn.execute(sql).first()[0]          # or will return user object
+    return result                        
+
+if __name__ == '__main__':
+    print(attemptLogin("potterd123@gmail.com", "qqqqqq"))
+    #print(deleteUser("potterd123@gmail.com"))
