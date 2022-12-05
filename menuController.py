d287a1dba57ff9b9966c35768caddb818628000f
Brandon Huynh <brandon.huynh@live.com>
2022-12-05 15:13:50 -0800

    Corrected v1 comments, consolidated menu queries

--- menuController.py
+++ menuController.py
@@ -1,12 +1,26 @@
-from backendDriver import good_id_by_name, menu_items
+import sqlalchemy
+from connection import get_connection
 
 
 def getMenuItems():
-    return menu_items()
+    conn = get_connection()
+    sql = "SELECT Name, Cost FROM goods"
+
+    result = conn.execute(sqlalchemy.text(sql))
+
+    menu = []
+
+    for row in result:
+        menu.append(row)
+
+    return menu
 
 
-def getItemIdByName(name):
-    return good_id_by_name(name)
+def getItemIdByName(item):
+    conn = get_connection()
+    result = conn.execute(sqlalchemy.text("SELECT gid FROM goods WHERE Name = :i"), [{"i": item}]).scalar()
+    
+    return result
 
 
 
